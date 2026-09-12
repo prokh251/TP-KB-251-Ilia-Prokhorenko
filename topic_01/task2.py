a0caced9a79cab6c34675c12e3b047093ce6cd4b
prokh251 <zeusprout@gmail.com>
2026-09-12 17:36:36 +0300

    add report file

--- topic_01/task2.py
+++ topic_01/task2.py
@@ -1,7 +1,7 @@
 str = " пРоХОренКо іЛЬя "
 print("Початковий рядок:       ", f"'{str}'""\n")
 
-print("1. Метод strip():       ", f"'{str}'")
+print("1. Метод strip():        ", f"'{str.strip()}'")
 print("2. Метод capitalize():   ", f"'{str.strip().capitalize()}'") #str.strip() для того, щоб перший символ не був пробілом
 print("3. Метод title():       ", f"'{str.title()}'")
 print("4. Метод upper():       ", f"'{str.upper()}'")
